0b77c2645e93c10e88bfab8bba1281352251055c
Om Kumar <omgreat155@gmail.com>
2024-12-23 20:00:10 +0530

    fixed routing

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -13,11 +13,12 @@ import { verifyAuthToken } from '../middlewares/auth'
 const userRouter: Router = express.Router()
 const userController = new UserController()
 
+userRouter.post('/login', userController.loginWithEmailPassword)
 userRouter.post('/profile', verifyAuthToken, validate(validateCreateUserProfile as any), userController.createUserProfile)
 userRouter.put('/profile/:id', verifyAuthToken, validate(validateUpdateUserProfile as any), userController.updateUserProfile)
 userRouter.get('/profile/:firebaseId', validate(validateGetUserByFirebaseId as any), userController.getUserByFirebaseId)
 userRouter.get('/check-username/:userName', verifyAuthToken, validate(validateCheckUserNameExists as any), userController.checkUserNameExists)
 userRouter.get('/:id', verifyAuthToken, validate(validateGetUserById as any), userController.getUserById)
-userRouter.post('/login', userController.loginWithEmailPassword)
+
 export default userRouter
 
